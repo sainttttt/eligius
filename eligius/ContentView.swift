3d66aa3dda4ea68800b7d2adc448203f54d74201
saint <sainttttt@x265.ca>
2024-03-20 15:38:00 -0400

    countdown timer with menubar and global hotkey works now

--- eligius/ContentView.swift
+++ eligius/ContentView.swift
@@ -5,20 +5,20 @@
 //  Created by Saint on 3/14/24.
 //
 
+import HotKey
 import SwiftUI
 
 extension Color {
     init(hex: UInt, alpha: Double = 1) {
         self.init(
             .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 08) & 0xFF) / 255,
+            blue: Double((hex >> 00) & 0xFF) / 255,
             opacity: alpha
         )
     }
 }
-
 
 func getTime() -> String {
     let formatter = DateFormatter()
@@ -27,57 +27,121 @@ func getTime() -> String {
     return dateString
 }
 
+func isTimeInInterval(now: Date, timer: Bool) -> Bool {
+    let calendar = Calendar.current
 
-let winWidth = CGFloat(200)
-let winHeight = CGFloat(100)
+    // Get the minute component of the current time
+    let minuteComponent = calendar.component(.minute, from: now)
+    let secondComponent = calendar.component(.second, from: now)
+
+    // flash the minute component
+    if timer {
+        return minuteComponent % 5 == 0 && secondComponent % 2 == 0 && secondComponent > 50
+    } else {
+        return minuteComponent % 15 == 0 && secondComponent % 2 == 0 && secondComponent < 40
+    }
+}
+
+let winWidth = CGFloat(320)
+let winHeight = CGFloat(150)
 struct ContentView: View {
+    @EnvironmentObject var settings: Settings
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var now = getTime()
+    @State var timeColor = Color(hex: 0x6D071A)
+
+    var hotKey = HotKey(key: .l, modifiers: [.command, .option])
+
+    init() {}
 
     var body: some View {
         ZStack {
             Text("\(now)")
 
-                .font(Font.custom("EPSON-FUTO-MINCHO", size: 80))
-                //.foregroundColor(Color(hex: 0x650015))
-                .foregroundColor(Color(hex: 0x6D071A))
-                //.font(Font.custom("Ac437_Apricot_Mono", size: 80))
-                //.font(Font.custom("OPTICaslon-ExtraCondensed", size: 40))
+                .font(Font.custom("EPSON-FUTO-MINCHO", size: 120))
+                // .foregroundColor(Color(hex: 0x650015))
+                .foregroundColor(timeColor)
+                // .font(Font.custom("Ac437_Apricot_Mono", size: 80))
+                // .font(Font.custom("OPTICaslon-ExtraCondensed", size: 40))
                 .dragWndWithClick()
-                .onReceive(timer) { input in
-                    self.now = getTime()
+               .onReceive(timer) { _ in
+                    // if settings.timerLeft != "" {
+                    //     let calendar = Calendar.current
+                    //     // Define the date components
+                    //     var dateComponents = DateComponents()
+                    //     // Set the minute component to 54
+                    //     dateComponents.minute = Int(timerLeft)
+                    //     let dDate =  calendar.date(byAdding: dateComponents, to: Date()
+                    // }
+
+
+                    var now = Date()
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "h:mm"
+                    print("receive")
+                    print(settings.timerDate)
+                    if (settings.timerDate != nil) {
+                        let secondsLeft = settings.timerDate!.timeIntervalSince(Date())
+                        if (secondsLeft < 1) {
+                            settings.timerDate = nil
+                            settings.timerLeft = ""
+                        } else {
+                            settings.timerLeft = String(Int(secondsLeft / 60))
+                            var dateComponents = DateComponents()
+                            dateComponents.second = Int(secondsLeft + 60)
+                            let calendar = Calendar.current
+                            let midnight = calendar.startOfDay(for: Date())
+                            now = calendar.date(byAdding: dateComponents, to: midnight)!
+                            formatter.dateFormat = "H:mm"
+                        }
+                    }
+                    let dateString = formatter.string(from: now)
+                    self.now = dateString
+
+                   if isTimeInInterval(now: now, timer: settings.timerDate != nil) {
+                        self.timeColor = Color(hex: 0xCCCCCC)
+                    } else {
+                        self.timeColor = Color(hex: 0x6D071A)
+                    }
                 }
 
-            //Text("Jn 3:38 whoever does not")
+            // Text("Jn 3:38 whoever does not")
             //    .font(Font.custom("EPSON-FUTO-MINCHO", size: 30))
             //    //.foregroundColor(Color(hex: 0x650015))
             //    .foregroundColor(Color(hex: 0x6D071A))
-
         }
-            .frame( minWidth: winWidth, maxWidth: winWidth,
-            minHeight: winHeight, maxHeight: winHeight)
-            .background(Color.black.opacity(0.3))
-            .overlay(AcceptingFirstMouse()) // must be on top (no confuse, it is transparent)
-
+        .frame(minWidth: winWidth, maxWidth: winWidth,
+               minHeight: winHeight, maxHeight: winHeight)
+        .background(Color.black.opacity(0.3))
+        .cornerRadius(15)
+        .dragWndWithClick()
+        .overlay(AcceptingFirstMouse()) // must be on top (no confuse, it is transparent)
+        .onAppear {
+            print("meow")
+            hotKey.keyDownHandler = {
+                print("Pressed at \(Date())")
+                settings.isMenuPresented.toggle()
+            }
+        }
     }
 }
 
 import Cocoa
 
 // Just mouse accepter
-class MyViewView : NSView {
-    override func acceptsFirstMouse(for event: NSEvent?) -> Bool {
+class MyViewView: NSView {
+    override func acceptsFirstMouse(for _: NSEvent?) -> Bool {
         return true
     }
 }
 
 // Representable wrapper (bridge to SwiftUI)
-struct AcceptingFirstMouse : NSViewRepresentable {
-    func makeNSView(context: NSViewRepresentableContext<AcceptingFirstMouse>) -> MyViewView {
+struct AcceptingFirstMouse: NSViewRepresentable {
+    func makeNSView(context _: NSViewRepresentableContext<AcceptingFirstMouse>) -> MyViewView {
         return MyViewView()
     }
 
-    func updateNSView(_ nsView: MyViewView, context: NSViewRepresentableContext<AcceptingFirstMouse>) {
+    func updateNSView(_ nsView: MyViewView, context _: NSViewRepresentableContext<AcceptingFirstMouse>) {
         nsView.setNeedsDisplay(nsView.bounds)
     }
 
@@ -86,7 +150,7 @@ struct AcceptingFirstMouse : NSViewRepresentable {
 
 func chromeless() {
     if let window = NSApp.windows.first {
-        //hide buttons
+        // hide buttons
         window.standardWindowButton(.closeButton)?.isHidden = true
         window.standardWindowButton(.miniaturizeButton)?.isHidden = true
         window.standardWindowButton(.zoomButton)?.isHidden = true
@@ -103,15 +167,13 @@ func chromeless() {
     }
 }
 
-
-
 import Foundation
 import SwiftUI
 
 @available(OSX 11.0, *)
 extension View {
     func dragWndWithClick() -> some View {
-        self.overlay(DragWndView())
+        overlay(DragWndView())
     }
 }
 
@@ -123,24 +185,24 @@ struct DragWndView: View {
     }
 
     var body: some View {
-        ( test ? Color.green : Color.clickableAlpha )
-            .overlay( DragWndNSRepr() )
+        (test ? Color.green : Color.clickableAlpha)
+            .overlay(DragWndNSRepr())
     }
 }
 
 ///////////////
-///HELPERS
+/// HELPERS
 ///////////////
 
-fileprivate struct DragWndNSRepr: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSView {
+private struct DragWndNSRepr: NSViewRepresentable {
+    func makeNSView(context _: Context) -> NSView {
         return DragWndNSView()
     }
 
-    func updateNSView(_ nsView: NSView, context: Context) { }
+    func updateNSView(_: NSView, context _: Context) {}
 }
 
-fileprivate class DragWndNSView: NSView {
+private class DragWndNSView: NSView {
     override public func mouseDown(with event: NSEvent) {
         window?.performDrag(with: event)
     }
@@ -150,23 +212,23 @@ fileprivate class DragWndNSView: NSView {
 public extension Color {
     init(hex: UInt32) {
         self.init(
-            red:       Double((hex >> 16) & 0xFF) / 256.0,
-            green:     Double((hex >> 8) & 0xFF) / 256.0,
-            blue:      Double(hex & 0xFF) / 256.0
+            red: Double((hex >> 16) & 0xFF) / 256.0,
+            green: Double((hex >> 8) & 0xFF) / 256.0,
+            blue: Double(hex & 0xFF) / 256.0
         )
     }
 
     init(rgbaHex: UInt32) {
         self.init(
-            red:      Double((rgbaHex >> 24) & 0xFF) / 256.0,
-            green:    Double((rgbaHex >> 16) & 0xFF) / 256.0,
-            blue:     Double((rgbaHex >> 8) & 0xFF) / 256.0,
-            opacity:  Double(rgbaHex & 0xFF) / 256.0
+            red: Double((rgbaHex >> 24) & 0xFF) / 256.0,
+            green: Double((rgbaHex >> 16) & 0xFF) / 256.0,
+            blue: Double((rgbaHex >> 8) & 0xFF) / 256.0,
+            opacity: Double(rgbaHex & 0xFF) / 256.0
         )
     }
 }
 
 @available(OSX 10.15, *)
 public extension Color {
-    static var clickableAlpha: Color { get { return Color(rgbaHex: 0x01010101) } }
+    static var clickableAlpha: Color { return Color(rgbaHex: 0x0101_0101) }
 }
--- eligius/eligiusApp.swift
+++ eligius/eligiusApp.swift
@@ -5,45 +5,99 @@
 //  Created by Saint on 3/14/24.
 //
 
+import MenuBarExtraAccess
 import SwiftUI
-
+import Combine
 
 let appSize = CGFloat(200)
+
+class Settings: ObservableObject {
+    @Published var isMenuPresented: Bool = false
+    @Published var timerLeft = ""
+    @Published var timerDate: Date?
+}
 
 @main
 struct eligiusApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    @State var currentNumber: String = "1"
+    @StateObject var settings = Settings()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .frame(
-                    minWidth: appSize, maxWidth: appSize,
-                    minHeight: appSize, maxHeight: appSize)
-        }.windowResizability(.contentSize)
+                    minWidth: 350, maxWidth: 350,
+                    minHeight: 200, maxHeight: 200
+                )
+                .environmentObject(settings)
+        }
+        .windowResizability(.contentSize)
 
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("MyApp Menu", systemImage: "folder") {
+            MyMenu()
+                .introspectMenuBarExtraWindow { window in // <-- the magic ✨
+                    window.animationBehavior = .alertPanel
+
+                }
+                .environmentObject(settings)
         }
 
-        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
-            // 3
-            Button("One") {
-                currentNumber = "1"
-            }
-            Button("Two") {
-                currentNumber = "2"
-            }
-            Button("Three") {
-                currentNumber = "3"
-            }
+        .menuBarExtraStyle(.window)
+        .menuBarExtraAccess(isPresented: $settings.isMenuPresented) { _ in // <-- the magic ✨
+            // access status item or store it in a @State var
         }
     }
 }
 
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
+    }
+}
+
+struct MyMenu: View {
+    @EnvironmentObject var settings: Settings
+
+    var body: some View {
+        VStack {
+            Text("Timer:")
+                .font(Font.custom("EPSON-FUTO-MINCHO", size: 50))
+            TextField("hint", text: $settings.timerLeft)
+                .font(Font.custom("EPSON-FUTO-MINCHO", size: 50))
+                .onReceive(Just(settings.timerLeft)) { newValue in
+                    let value = newValue.replacingOccurrences(of: "\\D", with: "", options: .regularExpression)
+                    if value != newValue {
+                        self.settings.timerLeft = value
+                    }
+                    print(newValue)
+                }
+                .onSubmit() {
+                    settings.isMenuPresented.toggle()
+                    let calendar = Calendar.current
+                    // Define the date components
+                    var dateComponents = DateComponents()
+                    // Set the minute component to 54
+                    dateComponents.minute = Int(self.settings.timerLeft)
+                    self.settings.timerDate =  calendar.date(byAdding: dateComponents, to: Date())
+                    print(self.settings.timerLeft)
+                    print("\(self.settings.timerDate)")
+                }
+        }
+        .frame(
+            minWidth: 350, maxWidth: 350,
+            minHeight: 200, maxHeight: 200
+        )
+    }
+}
+
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
+    func applicationDidFinishLaunching(_: Notification) {
         chromeless()
     }
 }
