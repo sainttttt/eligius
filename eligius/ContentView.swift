678ec0f4e868f92024f6e13723df6de42b6bb811
saint <sainttttt@x265.ca>
2024-05-07 09:51:50 -0400

    window stays in center unless clicked or hotkey, disabled cmd-w or cmd-q quit

--- eligius/ContentView.swift
+++ eligius/ContentView.swift
@@ -7,6 +7,7 @@
 
 import HotKey
 import SwiftUI
+import Combine
 
 extension Color {
     init(hex: UInt, alpha: Double = 1) {
@@ -27,30 +28,45 @@ func getTime() -> String {
     return dateString
 }
 
-func isTimeInInterval(now: Date, timer: Bool) -> Bool {
+func isTimeInInterval(now: Date, timer: Bool) -> [String: Bool] {
     let calendar = Calendar.current
 
     // Get the minute component of the current time
     let minuteComponent = calendar.component(.minute, from: now)
     let secondComponent = calendar.component(.second, from: now)
 
+    var flash = false
+    var center = false
     // flash the minute component
     if timer {
-        return minuteComponent % 5 == 0 && secondComponent % 2 == 0 && secondComponent > 50
+        // flash = minuteComponent % 5 == 0 && secondComponent % 2 == 0 && secondComponent > 50
+        flash = minuteComponent % 5 == 0 && secondComponent > 50
+        center = minuteComponent % 5 == 0 && secondComponent == 50
     } else {
-        return minuteComponent % 15 == 0 && secondComponent % 2 == 0 && secondComponent < 40
+        // flash = minuteComponent % 15 == 0 && secondComponent % 2 == 0 && secondComponent < 40
+        flash = minuteComponent % 15 == 0
+        center = minuteComponent % 15 == 0 && secondComponent == 0
     }
+
+    return ["flash":flash,
+        "center": center]
 }
 
 let winWidth = CGFloat(320)
 let winHeight = CGFloat(150)
 struct ContentView: View {
     @EnvironmentObject var settings: Settings
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 0.3, on: .main, in: .common).autoconnect()
     @State var now = getTime()
     @State var timeColor = Color(hex: 0x6D071A)
 
-    var hotKey = HotKey(key: .l, modifiers: [.command, .option])
+    @State var window : NSWindow?
+    @State var originalPosition : NSRect?
+    @State private var cancellables = Set<AnyCancellable>()
+
+    var hotKey = HotKey(key: .minus, modifiers: [.command, .shift])
+    var hkShowTimer = HotKey(key: .equal, modifiers: [.command, .shift])
+    var hkRestorePosition = HotKey(key: .equal, modifiers: [.command, .option])
 
     init() {}
 
@@ -64,30 +80,22 @@ struct ContentView: View {
                 // .font(Font.custom("Ac437_Apricot_Mono", size: 80))
                 // .font(Font.custom("OPTICaslon-ExtraCondensed", size: 40))
                 .dragWndWithClick()
-               .onReceive(timer) { _ in
-                    // if settings.timerLeft != "" {
-                    //     let calendar = Calendar.current
-                    //     // Define the date components
-                    //     var dateComponents = DateComponents()
-                    //     // Set the minute component to 54
-                    //     dateComponents.minute = Int(timerLeft)
-                    //     let dDate =  calendar.date(byAdding: dateComponents, to: Date()
-                    // }
+                .onReceive(timer) { _ in
 
+                    chromeless()
 
-                  chromeless()
-                   var now = Date()
+                    // now is the time that we display. So if we want to display
+                    // another time, such as the countdown timer, we should change
+                    // the now variable
+                    var now = Date()
                     let formatter = DateFormatter()
                     formatter.dateFormat = "h:mm"
-                    print("receive")
-                    print(settings.timerDate)
-                    if (settings.timerDate != nil) {
+                    if (settings.timerDate != nil && settings.showTimer) {
                         let secondsLeft = settings.timerDate!.timeIntervalSince(Date())
-                        if (secondsLeft < 1) {
+                        if secondsLeft < 1 {
                             settings.timerDate = nil
-                            settings.timerLeft = ""
                         } else {
-                            settings.timerLeft = String(Int(secondsLeft / 60))
+                            ///settings.timerLeft = String(Int(secondsLeft / 60) + 1)
                             var dateComponents = DateComponents()
                             dateComponents.second = Int(secondsLeft + 60)
                             let calendar = Calendar.current
@@ -98,12 +106,23 @@ struct ContentView: View {
                     }
                     let dateString = formatter.string(from: now)
                     self.now = dateString
+                    if settings.showTimer {
+                        self.now = "-\(self.now)"
+                    }
 
-                   if isTimeInInterval(now: now, timer: settings.timerDate != nil) {
+                    let timeIntervalRes = isTimeInInterval(now: now, timer: settings.timerDate != nil)
+                    if timeIntervalRes["flash"]! {
                         self.timeColor = Color(hex: 0xCCCCCC)
                     } else {
                         self.timeColor = Color(hex: 0x6D071A)
                     }
+
+                    if timeIntervalRes["center"]! {
+                        centerWindow()
+                    }
+                    // else {
+                    //     restoreWindowPos()
+                    // }
                 }
 
             // Text("Jn 3:38 whoever does not")
@@ -111,21 +130,90 @@ struct ContentView: View {
             //    //.foregroundColor(Color(hex: 0x650015))
             //    .foregroundColor(Color(hex: 0x6D071A))
         }
-        .frame(minWidth: winWidth, maxWidth: winWidth,
-               minHeight: winHeight, maxHeight: winHeight)
-        .background(Color.black.opacity(0.3))
-        .cornerRadius(15)
-        .dragWndWithClick()
-        .overlay(AcceptingFirstMouse()) // must be on top (no confuse, it is transparent)
-        .onAppear {
-            print("meow")
-            hotKey.keyDownHandler = {
-                print("Pressed at \(Date())")
-                settings.isMenuPresented.toggle()
+            .frame(minWidth: winWidth, maxWidth: winWidth,
+            minHeight: winHeight, maxHeight: winHeight)
+            .background(Color.black.opacity(0.3))
+            .cornerRadius(15)
+            .dragWndWithClick()
+            .overlay(AcceptingFirstMouse()) // must be on top (no confuse, it is transparent)
+            .onAppear {
+                print("meow")
+                hotKey.keyDownHandler = {
+                    print("Pressed at \(Date())")
+                    settings.isMenuPresented.toggle()
+                }
+
+                hkShowTimer.keyDownHandler = {
+                    settings.showTimer.toggle()
+                }
+
+                hkRestorePosition.keyDownHandler = {
+                    restoreWindowPos()
+                }
+
             }
-        }
+            .onTapGesture {
+                restoreWindowPos()
+            }
+            .background(WindowAccessor { newWindow in
+                if let newWindow = newWindow {
+                    monitorVisibility(window: newWindow)
+
+                } else {
+                    // window closed: release all references
+                    self.window = nil
+                    self.cancellables.removeAll()
+                }
+            })
     }
+
+    private func monitorVisibility(window: NSWindow) {
+        window.publisher(for: \.isVisible)
+            .dropFirst()  // we know: the first value is not interesting
+            .sink(receiveValue: { isVisible in
+                if isVisible {
+                    self.window = window
+                    //placeWindow(window)
+                }
+            })
+            .store(in: &cancellables)
+    }
+
+    private func centerWindow() {
+        if self.window == nil {
+            return
+        }
+        let main = NSScreen.main!
+        let visibleFrame = main.visibleFrame
+        let windowSize = self.window!.frame.size
+
+        let windowX = visibleFrame.midX - windowSize.width/2
+        let windowY = visibleFrame.midY - windowSize.height/2
+
+        if self.window!.frame.origin.x != windowX &&
+            self.window!.frame.origin.y != windowY {
+            self.originalPosition = self.window!.frame
+        }
+
+        let desiredOrigin = CGPoint(x: windowX, y: windowY)
+        self.window?.setFrameOrigin(desiredOrigin)
+    }
+
+    private func restoreWindowPos() {
+        if self.window == nil {
+            return
+        }
+
+        if self.originalPosition == nil {
+            return
+        }
+
+        self.window!.setFrame(self.originalPosition!, display: true)
+        self.originalPosition = nil
+    }
+
 }
+
 
 import Cocoa
 
@@ -150,21 +238,24 @@ struct AcceptingFirstMouse: NSViewRepresentable {
 }
 
 func chromeless() {
-    if let window = NSApp.windows.first {
+    for window in NSApp.windows {
+    //if let window = NSApp.windows.first {
         // hide buttons
-        window.standardWindowButton(.closeButton)?.isHidden = true
-        window.standardWindowButton(.miniaturizeButton)?.isHidden = true
-        window.standardWindowButton(.zoomButton)?.isHidden = true
+        if (window.title == "eligius") {
+            window.standardWindowButton(.closeButton)?.isHidden = true
+            window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+            window.standardWindowButton(.zoomButton)?.isHidden = true
 
-        // hide title and bar
-        window.titleVisibility = .hidden
-        window.titlebarAppearsTransparent = true
+            // hide title and bar
+            window.titleVisibility = .hidden
+            window.titlebarAppearsTransparent = true
 
-        window.isOpaque = false
-        window.backgroundColor = NSColor.clear
-        window.level = .floating
-        window.collectionBehavior = .canJoinAllSpaces
-        window.hasShadow = false
+            window.isOpaque = false
+            window.backgroundColor = NSColor.clear
+            window.level = .floating
+            window.collectionBehavior = [.canJoinAllSpaces, .stationary, .fullScreenAuxiliary]
+            window.hasShadow = false
+        }
     }
 }
 
--- eligius/eligiusApp.swift
+++ eligius/eligiusApp.swift
@@ -13,8 +13,9 @@ let appSize = CGFloat(200)
 
 class Settings: ObservableObject {
     @Published var isMenuPresented: Bool = false
-    @Published var timerLeft = ""
+    @Published var timerString = ""
     @Published var timerDate: Date?
+    @Published var showTimer: Bool = false
 }
 
 @main
@@ -68,24 +69,58 @@ struct MyMenu: View {
         VStack {
             Text("Timer:")
                 .font(Font.custom("EPSON-FUTO-MINCHO", size: 50))
-            TextField("hint", text: $settings.timerLeft)
+            TextField("hint", text: $settings.timerString)
                 .font(Font.custom("EPSON-FUTO-MINCHO", size: 50))
-                .onReceive(Just(settings.timerLeft)) { newValue in
-                    let value = newValue.replacingOccurrences(of: "\\D", with: "", options: .regularExpression)
+                .onReceive(Just(settings.timerString)) { newValue in
+                    let value = newValue.replacingOccurrences(of: "[^0-9:]", with: "", options: .regularExpression)
                     if value != newValue {
-                        self.settings.timerLeft = value
+                        self.settings.timerString = value
                     }
-                    print(newValue)
                 }
                 .onSubmit() {
                     settings.isMenuPresented.toggle()
-                    let calendar = Calendar.current
-                    // Define the date components
-                    var dateComponents = DateComponents()
-                    // Set the minute component to 54
-                    dateComponents.minute = Int(self.settings.timerLeft)
-                    self.settings.timerDate =  calendar.date(byAdding: dateComponents, to: Date())
-                    print(self.settings.timerLeft)
+
+                    var moreMinutes = Int(self.settings.timerString)
+                    if self.settings.timerString.range(of: "[0-9]{1,2}:[0-9]{1,2}",
+                        options: .regularExpression, range: nil, locale: nil) != nil {
+
+                        print("time found")
+                        let timeArr = self.settings.timerString.components(separatedBy: ":")
+                        var inputHour = Int(timeArr[0])!
+                        let inputMin = Int(timeArr[1])!
+
+                        let curCalendar = Calendar.current
+                        var now = Date()
+                        // Get the minute component of the current time
+                        let currentHour = curCalendar.component(.hour, from: now)
+
+                        self.settings.timerDate = Calendar.current.date(bySettingHour: inputHour,
+                            minute: inputMin,
+                            second: 0, of: Date())!
+
+                        print(self.settings.timerDate)
+
+                        // add 12 hours if roll over
+                        if inputHour < Int(currentHour) {
+                            var dateComponents = DateComponents()
+                            let calendar = Calendar.current
+                            dateComponents.hour = 12
+                            self.settings.timerDate =  calendar.date(byAdding: dateComponents,
+                                                                     to: self.settings.timerDate!)
+                        }
+
+                    } else {
+                        var dateComponents = DateComponents()
+                        let calendar = Calendar.current
+                        dateComponents.minute = moreMinutes
+                        self.settings.timerDate =  calendar.date(byAdding: dateComponents, to: Date())
+                    }
+
+
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "h:mm"
+                    let dateString = formatter.string(from: self.settings.timerDate!)
+                    self.settings.timerString = dateString
                     print("\(self.settings.timerDate)")
                 }
         }
@@ -103,5 +138,14 @@ struct MyMenu: View {
 final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_: Notification) {
         chromeless()
+
+    }
+    func applicationWillUpdate(_ notification: Notification) {
+
+        DispatchQueue.main.async {
+            if let menu = NSApplication.shared.mainMenu {
+                menu.items.removeAll()
+            }
+        }
     }
 }
